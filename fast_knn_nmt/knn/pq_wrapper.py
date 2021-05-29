57944110a63dc1b3c15ed3cc2d549ccdbe0ee68b
mengyuxian <mengyuxian@shannonai.com>
2021-05-29 13:55:29 +0800

    FEA/quantizer decode in parallel

--- fast_knn_nmt/knn/pq_wrapper.py
+++ fast_knn_nmt/knn/pq_wrapper.py
@@ -1,8 +1,5 @@
 # encoding: utf-8
 """
-
-
-
 @desc: https://gist.github.com/mdouze/94bd7a56d912a06ac4719c50fa5b01ac
 
 """
@@ -87,14 +84,14 @@ class TorchPQCodec(NumpyPQCodec, torch.nn.Module):
             self.register_buffer("b", torch.from_numpy(b))
         else:
             self.pre_torch = False
-        self.register_buffer("centroids_torch", torch.from_numpy(self.centroids))
-        self.register_buffer("norm2_centroids_torch", torch.from_numpy(self.norm2_centroids))
+        self.register_buffer("centroids_torch", torch.from_numpy(self.centroids))  # [M, ksub, dsub]
+        self.register_buffer("norm2_centroids_torch", torch.from_numpy(self.norm2_centroids))  # [M, ksub]
 
     def encode(self, x):
         """
 
         Args:
-            x: [n, M]
+            x: [n, D], where D = M * dsub. M is the number of subspaces, dsub is the dimension of each subspace
 
         Returns:
 
@@ -106,26 +103,56 @@ class TorchPQCodec(NumpyPQCodec, torch.nn.Module):
                 x += b
 
         n, d = x.shape
-        cen = self.centroids_torch
+        cen = self.centroids_torch  # [M, ksub, dsub]
         M, ksub, dsub = cen.shape
-        codes = torch.empty((n, M), dtype=torch.uint8, device=x.device)
-        # maybe possible to vectorize this loop...
-        for m in range(M):
-            # compute all per-centroid distances, ignoring the ||x||^2 term
-            xslice = x[:, m * dsub:(m + 1) * dsub]  # []
-            dis = self.norm2_centroids_torch[m] - 2 * xslice @ cen[m].t()
-            codes[:, m] = dis.argmin(dim=1)
+
+        # for loop version
+        # codes = torch.empty((n, M), dtype=torch.uint8, device=x.device)
+        # # maybe possible to vectorize this loop...
+        # for m in range(M):
+        #     # compute all per-centroid distances, ignoring thecen.shape ||x||^2 term
+        #     xslice = x[:, m * dsub:(m + 1) * dsub]  # [n, dsub]
+        #     dis = self.norm2_centroids_torch[m] - 2 * xslice @ cen[m].t()
+        #     codes[:, m] = dis.argmin(dim=1)
+
+        # parallel version
+        x = x.view(n, M, dsub).unsqueeze(-2)  # [n, M, 1, dsub]
+        norm = self.norm2_centroids_torch.unsqueeze(0)  # [1, M, ksub]
+        cen = cen.transpose(1, 2).unsqueeze(0)  # [1, M, dsub, ksub]
+        dot_product = torch.matmul(x, cen).squeeze(-2)  # [n, M, 1, ksub]
+        dis = norm - 2 * dot_product  # [n, M, ksub]
+        codes = dis.argmin(dim=2).to(torch.uint8)
+
         return codes
 
     def decode(self, codes):
+        """
+
+        Args:
+            codes: [n, M], where M is the number of subspaces
+        Returns:
+            feature: [n, D], where D = M * dsub, and dsub is the dimension of subspace
+
+        """
         n, MM = codes.shape
-        cen = self.centroids_torch
+        cen = self.centroids_torch   # [M, ksub, dsub]
         M, ksub, dsub = cen.shape
         assert MM == M
-        x = torch.empty((n, M * dsub), dtype=torch.float32, device=codes.device)
-        for m in range(M):  # todo: vectorize
-            xslice = cen[m, codes[:, m].long()]
-            x[:, m * dsub:(m + 1) * dsub] = xslice
+
+        # for loop version
+        # x = torch.empty((n, M * dsub), dtype=torch.float32, device=codes.device)
+        # for m in range(M):
+        #     xslice = cen[m, codes[:, m].long()]
+        #     x[:, m * dsub:(m + 1) * dsub] = xslice
+
+        # parallel version
+        # x[n, m, j] = cen[m][codes[n][m]][j]
+        x = torch.gather(
+            cen.unsqueeze(0).expand(n, -1, -1, -1),  # [n, M, ksub, dsub]
+            dim=2,
+            index=codes.long().unsqueeze(-1).unsqueeze(-1).expand(-1, -1, -1, dsub)  # [n, M, 1, dsub]
+        )   # [n, M, 1, dsub]
+        x = x.view(n, -1)
 
         if self.pre is not None:
             A, b = self.A, self.b
@@ -136,30 +163,25 @@ class TorchPQCodec(NumpyPQCodec, torch.nn.Module):
 
 
 if __name__ == '__main__':
-    # codec = faiss.index_factory(128, "OPQ32_128,PQ32")
-    codec = faiss.index_factory(128, "PCAR64,PQ32")
-    # train the codec
-    xt = faiss.rand((10000, 128))
-    codec.train(xt)
-    # convetert to a pure numpy codec
-    ncodec = NumpyPQCodec(codec)
-    # test encoding
-    xb = faiss.rand((100, 128), 1234)
-    (ncodec.encode(xb) == codec.sa_encode(xb)).all()
-    # test decoding
-    codes = faiss.randint((100, 32), vmax=256, seed=12345).astype('uint8')
-    np.allclose(codec.sa_decode(codes), ncodec.decode(codes))
-
+    codec = faiss.read_index("//data/yuxian/datasets/multi_domain_paper/it/bpe/de-en-bin/quantizer-decoder.new")
     # test encode
     dev = torch.device('cuda:0')
     tcodec = TorchPQCodec(codec).to(dev)
 
-    xb = torch.rand(1000, 128).to(dev)
+    xb = torch.rand(1000, 1024).to(dev)
     (tcodec.encode(xb).cpu().numpy() == codec.sa_encode(xb.cpu().numpy())).all()
     # test decode
-    codes = torch.randint(256, size=(1000, 32), dtype=torch.uint8).to(dev)
+    codes = torch.randint(256, size=(1000, 128), dtype=torch.uint8).to(dev)
     np.allclose(
         tcodec.decode(codes).cpu().numpy(),
         codec.sa_decode(codes.cpu().numpy()),
         atol=1e-6
     )
+
+    from tqdm import tqdm
+
+    for _ in tqdm(range(100)):
+        tcodec.encode(xb)
+
+    for _ in tqdm(range(100)):
+        tcodec.decode(codes)
